727a9ffa81c805582957042022465441a8f40f07
sergio-teruel <sergio.teruel@tecnativa.com>
2022-11-01 07:14:02 +0100

    [MIG] agreement_rebate: Migration to v15.0

--- agreement_rebate/__manifest__.py
+++ agreement_rebate/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Agreement Rebate",
     "summary": "Rebate in agreements",
-    "version": "13.0.1.0.1",
+    "version": "15.0.1.0.0",
     "development_status": "Beta",
     "category": "Contract",
     "website": "https://github.com/OCA/contract",
