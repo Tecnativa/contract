dc42cf8bef3912ca416e6bbb4ed54c85bbd8abdb
Carlos Lopez <carlos.lopez@tecnativa.com>
2024-12-05 10:51:57 -0500

    [IMP] blegal_custom: Add follower automatically from the analytic account

--- odoo/custom/src/private/blegal_custom/tests/__init__.py
+++ odoo/custom/src/private/blegal_custom/tests/__init__.py
@@ -3,3 +3,4 @@ from . import test_base_import_pdf_by_template_bga
 from . import test_base_import_pdf_by_template_bgbl
 from . import test_base_import_pdf_by_template_blegal
 from . import test_base_import_pdf_by_template_misc
+from . import test_reimbursement_cost
